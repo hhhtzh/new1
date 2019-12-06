fe02e521782bf5ca067d61ab4b175a36140b8076
hhhtzh <284171810@qq.com>
2019-12-06 02:22:17 -0800

    13

---
+++ week13/my.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
